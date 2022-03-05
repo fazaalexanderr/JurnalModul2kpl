12cb92c566f7eee5dae1b338759adde2cb6ccc17
fzTelU <fazaalexander@student.telkomuniversity.ac.id>
2022-03-05 11:18:17 +0700

    menambahkan implementasi kalkulator

--- modul2_1302204062/Form1.Designer.cs
+++ modul2_1302204062/Form1.Designer.cs
@@ -51,6 +51,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "1";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
@@ -60,6 +61,7 @@
             this.button1.TabIndex = 3;
             this.button1.Text = "2";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button3
             // 
@@ -69,6 +71,7 @@
             this.button3.TabIndex = 4;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -78,6 +81,7 @@
             this.button4.TabIndex = 5;
             this.button4.Text = "4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -87,6 +91,7 @@
             this.button5.TabIndex = 6;
             this.button5.Text = "7";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -96,6 +101,7 @@
             this.button6.TabIndex = 7;
             this.button6.Text = "5";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -105,6 +111,7 @@
             this.button7.TabIndex = 8;
             this.button7.Text = "9";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -114,6 +121,7 @@
             this.button8.TabIndex = 9;
             this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -123,42 +131,45 @@
             this.button9.TabIndex = 10;
             this.button9.Text = "6";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
-            this.button10.Location = new System.Drawing.Point(280, 200);
+            this.button10.Location = new System.Drawing.Point(280, 92);
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(40, 30);
             this.button10.TabIndex = 11;
             this.button10.Text = "+";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // button11
             // 
-            this.button11.Location = new System.Drawing.Point(317, 200);
+            this.button11.Location = new System.Drawing.Point(354, 92);
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(40, 30);
             this.button11.TabIndex = 12;
             this.button11.Text = "0";
             this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // button12
             // 
-            this.button12.Location = new System.Drawing.Point(354, 200);
+            this.button12.Location = new System.Drawing.Point(317, 92);
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(40, 30);
             this.button12.TabIndex = 13;
             this.button12.Text = "=";
             this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(277, 80);
+            this.label1.Location = new System.Drawing.Point(277, 52);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 16);
+            this.label1.Size = new System.Drawing.Size(0, 16);
             this.label1.TabIndex = 14;
-            this.label1.Text = "Output Label";
             // 
             // Form1
             // 
--- modul2_1302204062/Form1.cs
+++ modul2_1302204062/Form1.cs
@@ -12,9 +12,81 @@ namespace modul2_1302204062
 {
     public partial class Form1 : Form
     {
+        bool operation_pressed = false;
+        int[] bilangan = new int[5];
+        int jumlah, i = 0;
+        string result;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "1";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "4";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "5";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "6";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "8";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "9";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "0";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            bilangan[i] = Convert.ToInt32(label1.Text);
+            for (i = 0; i < bilangan.Length; i++)
+                jumlah = jumlah + bilangan[i];
+            result = jumlah.ToString();
+            label1.Text = result;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            operation_pressed = true;
+            if (operation_pressed == true)
+                bilangan[i] = Convert.ToInt32(label1.Text);
+                i++;
+                label1.Text = "";
         }
     }
 }
